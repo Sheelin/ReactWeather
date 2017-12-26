1ec7c2f58e92b41bfb424a2ff02ec87750032180
Fiona <finara.k@hotmail.com>
2017-12-27 00:11:27 +0100

    fix localhost

--- server.js
+++ server.js
@@ -2,19 +2,20 @@ var express = require('express');
 
 // Create our app
 var app = express();
-console.log(process.env.PORT);
 const PORT = process.env.PORT || 3000;
+const ENVIRONMENT = process.env.ENVIRONMENT || 'production';
 
 app.use(function (request, response, next){
-  if (request.headers['x-forwarded-proto'] === 'http'){
-    next();
+  if (request.headers['x-forwarded-proto'] === 'https'){
+    let host = request.hostname;
+    response.redirect('http://' + host + request.url);
   } else {
-    response.redirect('http://' + request.hostname + request.url);
+    next();
   }
 });
 
 app.use(express.static('public'));
 
 app.listen(PORT, function () {
-  console.log('Express server is up on port ' + PORT);
+  console.log('Express server is up on port ' + PORT + ', Environment is ' + ENVIRONMENT);
 });

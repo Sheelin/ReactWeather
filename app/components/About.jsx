430a519d2863fea349354fc66b3eb649e9d195a8
Fiona <finara.k@hotmail.com>
2017-12-27 01:08:18 +0100

    fill about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,6 +5,23 @@ var About = (props) => {
     <div>
       <h1 className="text-center">About</h1>
       <p>Welcome to my humble page!</p>
+      <p>
+        This is a weather application build on React. I have built this
+        with The Complete React Web App Developer Course.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+            JavaScript framework used.
+        </li>
+        <li>
+          <a href="http://openweathermap.org">Open Weather Map</a> - I used
+            Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
     </div>
   )
 };

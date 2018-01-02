43f938b630523d413fae7e082ce015f9663c63ad
Fiona <finara.k@hotmail.com>
2018-01-02 14:08:11 +0100

    add nav search functionality

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -7,7 +7,7 @@ var openWeatherMap = require('openWeatherMap');
 var Weather = React.createClass({
   getInitialState: function () {
     return {
-      isLoading:false
+      isLoading: false
     }
   },
   handleSearch: function (location) {
@@ -15,43 +15,62 @@ var Weather = React.createClass({
 
     this.setState({
       isLoading: true,
-      errorMessage: undefined
+      errorMessage: undefined,
+      location: undefined,
+      temp: undefined
     });
-    openWeatherMap.getTemp(location).then(function (temp){
+
+    openWeatherMap.getTemp(location).then(function (temp) {
       that.setState({
         location: location,
         temp: temp,
         isLoading: false
-      })
-    }, function (e){
+      });
+    }, function (e) {
       that.setState({
         isLoading: false,
         errorMessage: e.message
       });
     });
   },
+  componentDidMount: function () {
+    var location = this.props.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
+  componentWillReceiveProps: function (newProps) {
+    var location = newProps.location.query.location;
+
+    if (location && location.length > 0) {
+      this.handleSearch(location);
+      window.location.hash = '#/';
+    }
+  },
   render: function () {
     var {isLoading, temp, location, errorMessage} = this.state;
 
     function renderMessage () {
       if (isLoading) {
-        return <h3 className="text-center">Fetching Weather...</h3>;
+        return <h3 className="text-center">Fetching weather...</h3>;
       } else if (temp && location) {
-        return <WeatherMessage location={location} temp={temp}/>;
+        return <WeatherMessage temp={temp} location={location}/>;
       }
     }
 
     function renderError () {
-      if(typeof errorMessage === 'string'){
+      if (typeof errorMessage === 'string') {
         return (
-          <ErrorModal title="Oops..." message="I'm sorry I did not recognize this location :("/>
-        );
+          <ErrorModal message={errorMessage}/>
+        )
       }
     }
 
     return (
       <div>
-        <h1 className="text-center page-title">Weather</h1>
+        <h1 className="text-center page-title">Get Weather</h1>
         <WeatherForm onSearch={this.handleSearch}/>
         {renderMessage()}
         {renderError()}
